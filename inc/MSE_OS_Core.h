ee1acbe58ba0b63d965fb1b6fde8776c255712b3
juniormonroy <you@example.com>
2022-07-19 05:51:05 -0300

    Primera version

---
+++ inc/MSE_OS_Core.h
@@ -0,0 +1,96 @@
+/*
+ * MSE_OS_Core.h
+ *
+ *  Created on: Jul 19, 2022
+ *      Author: Junior
+ */
+
+#ifndef MSE_OS_INC_MSE_OS_CORE_H_
+#define MSE_OS_INC_MSE_OS_CORE_H_
+
+
+
+#endif /* MSE_OS_INC_MSE_OS_CORE_H_ */
+
+/*
+ * MSE_OS_Core.h
+ *
+ *  Created on: 26 mar. 2020
+ *      Author: gonza
+ */
+
+#ifndef ISO_I_2020_MSE_OS_INC_MSE_OS_CORE_H_
+#define ISO_I_2020_MSE_OS_INC_MSE_OS_CORE_H_
+
+#include <stdint.h>
+#include "board.h"
+
+
+/************************************************************************************
+ * 			Tamaño del stack predefinido para cada tarea expresado en bytes
+ ***********************************************************************************/
+
+#define STACK_SIZE 256
+
+//----------------------------------------------------------------------------------
+
+
+
+/************************************************************************************
+ * 	Posiciones dentro del stack de los registros que lo conforman
+ ***********************************************************************************/
+
+#define XPSR			1
+#define PC_REG			2
+#define LR				3
+#define R12				4
+#define R3				5
+#define R2				6
+#define R1				7
+#define R0				8
+#define LR_PREV_VALUE	9
+#define R4				10
+#define R5				11
+#define R6				12
+#define R7				13
+#define R8				14
+#define R9				15
+#define R10 			16
+#define R11 			17
+
+//----------------------------------------------------------------------------------
+
+
+/************************************************************************************
+ * 			Valores necesarios para registros del stack frame inicial
+ ***********************************************************************************/
+
+#define INIT_XPSR 	1 << 24				//xPSR.T = 1
+#define EXEC_RETURN	0xFFFFFFF9			//retornar a modo thread con MSP, FPU no utilizada
+
+//----------------------------------------------------------------------------------
+
+
+/************************************************************************************
+ * 						Definiciones varias
+ ***********************************************************************************/
+#define STACK_FRAME_SIZE			8
+#define FULL_STACKING_SIZE 			17	//16 core registers + valor previo de LR
+
+
+/*==================[definicion de datos externa]=================================*/
+
+extern uint32_t sp_tarea1;					//Stack Pointer para la tarea 1
+extern uint32_t sp_tarea2;					//Stack Pointer para la tarea 2
+
+
+/*==================[definicion de prototipos]=================================*/
+
+void os_InitTarea(void *tarea, uint32_t *stack, uint32_t *stack_pointer);
+void os_Init(void);
+
+
+
+
+
+#endif /* ISO_I_2020_MSE_OS_INC_MSE_OS_CORE_H_ */
